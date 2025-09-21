b81b15826361799659857f8533477ff72e0522db
Alan <guardavidas1987@gmail.com>
2025-09-21 12:22:24 -0300

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from '@astrojs/react';
-import tina from '@tinacms/cli/dist/cli';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react(), tina()],
+  integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
