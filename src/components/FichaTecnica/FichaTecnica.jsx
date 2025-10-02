433284930148ede4d200971e101e72b82a209cee
Alan <guardavidas1987@gmail.com>
2025-10-02 20:03:03 -0300

    se cambio de lugar el header de ficha tecnica

--- src/components/FichaTecnica/FichaTecnica.jsx
+++ src/components/FichaTecnica/FichaTecnica.jsx
@@ -81,7 +81,15 @@ export default function FichaTecnica({ property }) {
     <div className="max-w-7xl mx-auto p-4 font-sans grid grid-cols-1 lg:grid-cols-3 gap-8">
       {/* --- Left Column --- */}
       <div className="lg:col-span-2">
-        {/* --- Header Card --- */}
+      
+
+        {/* --- Gallery Card --- */}
+        <Card className="mb-8">
+          <CardBody className="p-0">
+            <Gallery images={gallery} />
+          </CardBody>
+        </Card>
+  {/* --- Header Card --- */}
         <Card className="mb-8">
           <CardBody className="p-6">
             <div className="flex justify-between items-start">
@@ -113,14 +121,6 @@ export default function FichaTecnica({ property }) {
             </div>
           </CardBody>
         </Card>
-
-        {/* --- Gallery Card --- */}
-        <Card className="mb-8">
-          <CardBody className="p-0">
-            <Gallery images={gallery} />
-          </CardBody>
-        </Card>
-
         {/* --- Feature Cards --- */}
         <div className="space-y-6">
           {propertyFeatures && propertyFeatures.length > 0 && (
