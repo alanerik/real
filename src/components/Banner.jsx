f4da84b19c9177eae41a850cf153d24628d5a2b8
Alan <guardavidas1987@gmail.com>
2025-11-21 01:25:43 -0300

    fix banner y propiedades destacadas

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,9 +5,9 @@ const Banner = () => {
         <div className="relative">
             <Card
                 className="w-full h-96 bg-cover bg-center"
-                style={{ backgroundImage: "url('/imgHeroBanner.webp')" }}
+                style={{ backgroundImage: "url('/bosques.webp')" }}
             >
-                <div className="absolute inset-0 bg-gradient-to-r from-green-500 to-transparent"></div>
+                <div className="absolute inset-0 bg-gradient-to-r from-green-800 to-transparent"></div>
                 <div className="relative h-full flex flex-col justify-center items-start text-white p-4">
                     <h2 className="text-3xl md:text-5xl font-bold w-full md:w-1/3 text-left">Te damos una maxima visibilidad con real state</h2>
                     <Button color="warning" size="lg" className="mt-6 text-lg md:text-2xl">
